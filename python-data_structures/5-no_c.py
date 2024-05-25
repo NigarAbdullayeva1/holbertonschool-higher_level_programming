129778ef12e224b110cb3af745b10b3065dd0f05
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-05-25 18:39:58 +0000

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.replace('C',''))
+    # Remove both 'C' and 'c' characters
+    my_new_string = my_string.translate({ord('C'): None, ord('c'): None})
+    return my_new_string
