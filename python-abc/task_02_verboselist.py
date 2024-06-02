a7997db0f76a2f6f1195cf3a40a49ca0be4eb193
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-06-02 17:53:47 +0000

    python

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,32 +1,28 @@
-#!/usr/bin/python3
-from abc import ABC, abstractmethod
-
+#!/usr/bin/env python3
+# task_02_verboselist.py
 
 class VerboseList(list):
+    """A list that prints notifications on modifications."""
 
-    def append(self, value):
-        print(f"Added [{value}] to the list.")
-        super().append(value)
+    def append(self, item):
+        """Add an item to the end of the list and print a notification."""
+        super().append(item)
+        print(f"Added [{item}] to the list.")
 
-    def extend(self, value):
-        print(f"Extended the list with [{len(value)}] items.")
-        super().extend(value)
+    def extend(self, iterable):
+        """Extend the list by appending elements from the iterable and
+          print a notification."""
+        super().extend(iterable)
+        print(f"Extended the list with [{len(iterable)}] items.")
 
-    def remove(self, value):
+    def remove(self, item):
+        """Remove the first occurrence of the item and print a notification."""
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
-        # Checking if item is in list
-        if value in self:
-            print(f"Removed [{value}] from the list.")
-        super().remove(value)
-
-    def pop(self, value=None):
-
-        # Checking if index is specified
-        if value is None:
-            item = super().pop()
-            print(f"Popped [{item}] from the list.")
-            return item
-        else:
-            item = super().pop(value)
-            print(f"Popped [{item}] from the list.")
-            return item
+    def pop(self, index=-1):
+        """Remove and return item at index (default last) and print a
+          notification."""
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return item
