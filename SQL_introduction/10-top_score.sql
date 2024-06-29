7630590f1037f1df9f1a51708538197c80bde882
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-06-29 17:45:50 +0000

    order by

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-select score, name from second_table order by score desc;
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table ORDER BY score DESC;
